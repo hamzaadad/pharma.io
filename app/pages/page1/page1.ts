ca308a4733a95f08a9318610de2c66cee6f4432e
Hamza <adadhamza@outlook.fr>
2016-09-18 00:42:36 +0100

    spalsh/intro screen

--- app/pages/page1/page1.ts
+++ app/pages/page1/page1.ts
@@ -1,22 +1,43 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { Slides } from 'ionic-angular';
+import { ViewChild } from '@angular/core';
+
 
 @Component({
   templateUrl: 'build/pages/page1/page1.html'
 })
 export class Page1 {
-  public step:string
+  public step:string;
+  public steps:string[]
+  public index:number
+  @ViewChild('splash') slider: Slides;
+
+  slidOptions = {
+     initialSlide: 0,
+     loop: true
+   };
   constructor(public navCtrl: NavController) {
-    this.step  = 'rapide';
-  }
-  move(className:string, delay: number){
-    setTimeout(function(){
-      let item = document.getElementsByClassName(className)[0].classList;
-      if(!item.contains('shown')){
-        item.add('shown')
-      }else{
-        item.remove('shown')
+    this.steps = ['rapide', 'simple', 'fiable'];
+    this.index = 0;
+    this.move(this.steps[this.index]);
+    this.index++
+
+    var self = this;
+    setInterval(function(){
+      if(self.index == self.steps.length){
+        self.index = 0;
       }
-    }, delay);
+      self.goToSlide();
+      //self.move(self.steps[self.index]);
+      self.index++
+    }, 2000);
+
+  }
+  goToSlide() {
+   this.slider.slideNext(500);
+  }
+  move(step:string){
+    this.step = step
   }
 }
